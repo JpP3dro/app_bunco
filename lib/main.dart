f1402660f8d70cab0a320a460fc05258e7bee1af
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-05-12 18:52:46 -0300

    Colocando os mascotes nos cantos

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center( // Envolve a Column com Center
+      body: Stack(
+        children: [
+          Center( // Envolve a Column com Center
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // Centraliza os filhos da Column verticalmente
           mainAxisSize: MainAxisSize.min, // Faz a Column ocupar o mínimo de espaço vertical
@@ -55,6 +57,51 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
+          // Imagem no canto superior esquerdo
+          Positioned(
+            top: 30,
+            left: 15,
+            child: Image.asset(
+              'assets/images/mascote1.png', // Substitua pelo caminho da sua imagem de canto
+              width: 100, // Ajuste o tamanho conforme necessário
+              height: 100, // Ajuste o tamanho conforme necessário
+            ),
+          ),
+
+          // Imagem no canto superior direito
+          Positioned(
+            top: 30,
+            right: 15,
+            child: Image.asset(
+              'assets/images/mascote2.png', // Substitua pelo caminho da sua imagem de canto
+              width: 100, // Ajuste o tamanho conforme necessário
+              height: 100, // Ajuste o tamanho conforme necessário
+            ),
+          ),
+
+          // Imagem no canto inferior esquerdo
+          Positioned(
+            bottom: 30,
+            left: 15,
+            child: Image.asset(
+              'assets/images/mascote3.png', // Substitua pelo caminho da sua imagem de canto
+              width: 100, // Ajuste o tamanho conforme necessário
+              height: 100, // Ajuste o tamanho conforme necessário
+            ),
+          ),
+
+          // Imagem no canto inferior direito
+          Positioned(
+            bottom: 30,
+            right: 15,
+            child: Image.asset(
+              'assets/images/mascote4.png', // Substitua pelo caminho da sua imagem de canto
+              width: 100, // Ajuste o tamanho conforme necessário
+              height: 100, // Ajuste o tamanho conforme necessário
+            ),
+          ),
+    ]
+    ),
     );
   }
 }
