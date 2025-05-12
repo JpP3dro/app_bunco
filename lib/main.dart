72879530f05551a43f2ab586f21784a204cd7f39
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-05-12 17:58:12 -0300

    Alterando cores

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
 
@@ -12,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tela Inicial',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,38 +27,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tela Inicial'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Center(
-            child: Padding(
+      backgroundColor: Colors.black,
+      body: Center( // Envolve a Column com Center
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Centraliza os filhos da Column verticalmente
+          mainAxisSize: MainAxisSize.min, // Faz a Column ocupar o mínimo de espaço vertical
+          children: [
+            const Padding(
               padding: EdgeInsets.all(20),
               child: Text(
                 'Bem-vindo ao Meu App!',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
+                  color: Colors.blue
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const TelaLogin()),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const TelaLogin()),
+                ),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text('Começar'),
               ),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 50),
-              ),
-              child: const Text('Ir para Segunda Tela'),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
