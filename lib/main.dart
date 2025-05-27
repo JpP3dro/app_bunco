ecd511d6749895e6e7384bb6be1bc33216f47f9f
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-05-27 18:03:54 -0300

    Transformando as imagens da tela inicial em SVG

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'screens/login.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,46 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AnimatedHomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 
-class AnimatedHomeScreen extends StatefulWidget {
-  const AnimatedHomeScreen({super.key});
-
-  @override
-  _AnimatedHomeScreenState createState() => _AnimatedHomeScreenState();
-}
-
-class _AnimatedHomeScreenState extends State<AnimatedHomeScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1), // Duração da animação
-      vsync: this,
-    );
-
-    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeOut,
-      ),
-    );
-
-    // Inicia a animação quando a tela é construída
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -66,22 +34,20 @@ class _AnimatedHomeScreenState extends State<AnimatedHomeScreen> with SingleTick
       body: Stack(
         children: [
           Center(
-            child: ScaleTransition( // Aplica a animação de escala à Column central
-              scale: _scaleAnimation,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Image.asset(
-                      'assets/images/bunco.png',
-                    ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: SvgPicture.asset(
+                    'assets/images/telainicial/bunco.svg',
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Directionality(
-                      textDirection: TextDirection.rtl,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Directionality(
+                    textDirection: TextDirection.rtl,
                     child: ElevatedButton.icon(
                       onPressed: () => Navigator.pushReplacement(
                         context,
@@ -89,11 +55,11 @@ class _AnimatedHomeScreenState extends State<AnimatedHomeScreen> with SingleTick
                           child: const TelaLogin(),
                           type: PageTransitionType.fade,
                           duration: const Duration(milliseconds: 1000),
-                        )
+                        ),
                       ),
                       icon: const Icon(
-                          Icons.arrow_back_rounded,
-                          color: Colors.white,
+                        Icons.arrow_back_rounded,
+                        color: Colors.white,
                         size: 30,
                       ),
                       style: ElevatedButton.styleFrom(
@@ -101,60 +67,53 @@ class _AnimatedHomeScreenState extends State<AnimatedHomeScreen> with SingleTick
                         backgroundColor: const Color(0xFF4EA5FF),
                       ),
                       label: Text(
-                          'Começar',
+                        'Começar',
                         style: GoogleFonts.baloo2(
                           color: Colors.white,
                           fontWeight: FontWeight.bold,
                           fontSize: 25,
                         ),
-                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
 
-          // Imagem no canto superior esquerdo
+          // Seções das imagens posicionadas
           Positioned(
             top: 30,
             left: 15,
-            child: Image.asset(
-              'assets/images/mascote1.png',
+            child: SvgPicture.asset(
+              'assets/images/telainicial/mascote1.svg',
               width: 100,
               height: 100,
             ),
           ),
-
-          // Imagem no canto superior direito
           Positioned(
             top: 30,
             right: 15,
             child: Image.asset(
-              'assets/images/mascote2.png',
+              'assets/images/telainicial/mascote2.png',
               width: 100,
               height: 100,
             ),
           ),
-
-          // Imagem no canto inferior esquerdo
           Positioned(
             bottom: 30,
             left: 15,
             child: Image.asset(
-              'assets/images/mascote3.png',
+              'assets/images/telainicial/mascote3.png',
               width: 100,
               height: 100,
             ),
           ),
-
-          // Imagem no canto inferior direito
           Positioned(
             bottom: 30,
             right: 15,
-            child: Image.asset(
-              'assets/images/mascote4.png',
+            child: SvgPicture.asset(
+              'assets/images/telainicial/mascote4.svg',
               width: 100,
               height: 100,
             ),
