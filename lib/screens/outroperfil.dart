da354dc08a32ebee7761fae65d0761a67cb64b94
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2025-06-27 18:13:04 -0300

    Personalizando tela de outro perfil

--- lib/screens/outroperfil.dart
+++ lib/screens/outroperfil.dart
@@ -46,249 +46,241 @@ class _TelaOutroPerfilState extends State<TelaOutroPerfil> {
     exibirDias = widget.usuario!['ofensiva'] == 1 ? "dia" : "dias";
     exibirVidas = widget.usuario!['vidas'] == 1 ? "vida" : "vidas";
     cards = [
-      _Cards(imagePath: 'assets/images/icone/icone-github.png', titulo: '${widget.usuario!['xp']} XP', subtitulo: 'Quantidade de XP'),
-      _Cards(imagePath: 'assets/images/icone/icone-instagram.png', titulo: '${widget.usuario!['ofensiva']} $exibirDias', subtitulo: 'Dias de ofensiva'),
-      _Cards(imagePath: 'assets/images/icone/icone-linkedin.png', titulo: '${widget.usuario!['vidas']} $exibirVidas', subtitulo: 'Quantidade de vidas'),
+      _Cards(imagePath: 'assets/images/icone/icone-xp.png', titulo: '${widget.usuario!['xp']} XP', subtitulo: 'Quantidade de XP'),
+      _Cards(imagePath: 'assets/images/icone/icone-ofensiva.png', titulo: '${widget.usuario!['ofensiva']} $exibirDias', subtitulo: 'Dias de ofensiva'),
+      _Cards(imagePath: 'assets/images/icone/icone-vida.png', titulo: '${widget.usuario!['vidas']} $exibirVidas', subtitulo: 'Quantidade de vidas'),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF0D141F),
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded, color: Color(0xFF1CB0F6), size: 30,),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Color(0xFF0D141F),
         title: Text(
           "Perfil de ${widget.usuario!['nome']}",
           style: GoogleFonts.baloo2(
-            color: Colors.black,
+            color: Color(0xFFB0C2DE),
             fontSize: 25,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            // 1) Foto circular + dois botões nas laterais
-            Row(
-              children: [
-                Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.blue,
-                          width: 1,
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xAA1CB0F6),
-                            spreadRadius: 2,
-                            blurRadius: 12,
-                            offset: const Offset(5, 13),
-                          ),
-                        ],
-                      ),
-                      child: CircleAvatar(
-                        radius: 60,
-                        backgroundColor: corFundo,
-                        child: ClipOval(
-                          child: Image.asset(
-                            fotoSelecionada,
-                            height: 130,
-                            width: 130,
-                          ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              // 1) Foto circular
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundColor: corFundo,
+                      child: ClipOval(
+                        child: Image.asset(
+                          fotoSelecionada,
+                          height: 160,
+                          width: 160,
                         ),
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(width: 16),
-                // 2) Texto ao lado da foto
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(widget.usuario!["nome"],
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 4),
-                    Text('@${widget.usuario!["username"]}', style: TextStyle(fontSize: 16, color: Colors.grey)),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 22),
-            Divider(
-              color: Colors.black,
-            ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  // 2) Texto ao lado da foto
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.usuario!["nome"],
+                        style: GoogleFonts.baloo2(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 22,
+                            color: Colors.white
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        '@${widget.usuario!["username"]}',
+                        style: GoogleFonts.baloo2(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF586892)
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 22),
+              Divider(
+                color: Color(0xFF1A263D),
+                thickness: 3,
+              ),
 
-            // 3) Botões sociais (círculos azuis)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if ((widget.usuario!['link_github'] ?? "").isNotEmpty) ...[
-                  TextButton(
-                    onPressed: () {
-                      abrirLink(url: widget.usuario!['link_github']);
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.github,
-                          color: Colors.black,
-                          size: 40,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          "GitHub",
-                          style: GoogleFonts.quicksand(
-                              color: Color(0xFF000000),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                ],
-                if ((widget.usuario!['link_instagram'] ?? "").isNotEmpty) ...[
-                  TextButton(
-                    onPressed: () {
-                      abrirLink(url: widget.usuario!['link_instagram']);
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.instagram,
-                          color: Colors.pink,
-                          size: 40,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          "Instagram",
-                          style: GoogleFonts.quicksand(
-                              color: Color(0xFF000000),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                ],
-                if ((widget.usuario!['link_linkedin'] ?? "").isNotEmpty) ...[
-                  TextButton(
-                    onPressed: () {
-                      abrirLink(url: widget.usuario!['link_linkedin']);
-                    },
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.linkedin,
-                          color: Colors.blueAccent,
-                          size: 40,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          "Linkedin",
-                          style: GoogleFonts.quicksand(
-                              color: Color(0xFF000000),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ],
-            ),
-
-            //4) Cards com as informações
-            ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(16),
-              itemCount: cards.length,
-              itemBuilder: (context, index) {
-                final card = cards[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      side: BorderSide(
-                        color: Color(0xFF1CB0F6),
-                        width: 1.5,
-                      ),
-                    ),
-                    child: Container(
-                      height: 100,
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xAA1CB0F6),
-                            spreadRadius: 2,
-                            blurRadius: 12,
-                            offset: const Offset(5, 13), // sombra pra baixo
-                          ),
-                        ],
-                      ),
-                      child: Row(
+              // 3) Botões sociais (círculos azuis)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if ((widget.usuario!['link_github'] ?? "").isNotEmpty) ...[
+                    TextButton(
+                      onPressed: () {
+                        abrirLink(url: widget.usuario!['link_github']);
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
                         children: [
-                          // Quadrado da imagem
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.asset(
-                              card.imagePath,
-                              width: 76,  // largura fixa
-                              height: double.infinity,
-                              fit: BoxFit.cover,
-                            ),
+                          Icon(
+                            FontAwesomeIcons.github,
+                            color: Color(0xFF1CB0F6),
+                            size: 40,
                           ),
-
-                          const SizedBox(width: 16),
-
-                          // Espaço para os textos
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  card.titulo,
-                                  style: const TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(
-                                  card.subtitulo,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey[700],
-                                  ),
-                                ),
-                              ],
+                          /*const SizedBox(height: 4),
+                          Text(
+                            "GitHub",
+                            style: GoogleFonts.quicksand(
+                                color: Color(0xFF000000),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700
                             ),
-                          ),
+                          ),*/
                         ],
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ],
+                    const SizedBox(width: 16),
+                  ],
+                  if ((widget.usuario!['link_instagram'] ?? "").isNotEmpty) ...[
+                    TextButton(
+                      onPressed: () {
+                        abrirLink(url: widget.usuario!['link_instagram']);
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.instagram,
+                            color: Color(0xFF1CB0F6),
+                            size: 40,
+                          ),
+                          /*const SizedBox(height: 4),
+                          Text(
+                            "Instagram",
+                            style: GoogleFonts.quicksand(
+                                color: Color(0xFF000000),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700
+                            ),
+                          ),*/
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                  ],
+                  if ((widget.usuario!['link_linkedin'] ?? "").isNotEmpty) ...[
+                    TextButton(
+                      onPressed: () {
+                        abrirLink(url: widget.usuario!['link_linkedin']);
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.linkedin,
+                            color: Color(0xFF1CB0F6),
+                            size: 40,
+                          ),
+                          /*const SizedBox(height: 4),
+                          Text(
+                            "Linkedin",
+                            style: GoogleFonts.quicksand(
+                                color: Color(0xFF000000),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700
+                            ),
+                          ),*/
+                        ],
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+
+              //4) Cards com as informações
+              ListView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(16),
+                itemCount: cards.length,
+                itemBuilder: (context, index) {
+                  final card = cards[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        height: 90,
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF1F2433),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            // Quadrado da imagem
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                card.imagePath,
+                                width: 100,
+                                height: 150,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    card.titulo,
+                                    style: GoogleFonts.baloo2(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w700,
+                                        color: index == 0 ? Color(0xFF9069CD) : index == 1 ? Color(0xFFFFC800) : Color(0xFFEA2B2B)
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    card.subtitulo,
+                                    style: GoogleFonts.baloo2(
+                                        fontSize: 16,
+                                        color: Color(0xFF586892),
+                                        fontWeight: FontWeight.w700
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
